ffdf1567fd695b596c944d084e91df1a7856a414
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2018-06-15 20:41:00 +0200

    reformat source files

--- GUI/UnitManager.cs
+++ GUI/UnitManager.cs
@@ -11,34 +11,41 @@
 using System;
 using System.Collections.Generic;
 
-namespace OpenHardwareMonitor.GUI {
+namespace OpenHardwareMonitor.GUI
+{
 
-  public enum TemperatureUnit {
-    Celsius = 0,
-    Fahrenheit = 1
-  }
-
-  public class UnitManager {
-
-    private PersistentSettings settings;
-    private TemperatureUnit temperatureUnit;
-
-    public UnitManager(PersistentSettings settings) {
-      this.settings = settings;
-      this.temperatureUnit = (TemperatureUnit)settings.GetValue("TemperatureUnit",
-        (int)TemperatureUnit.Celsius);
+    public enum TemperatureUnit
+    {
+        Celsius = 0,
+        Fahrenheit = 1
     }
 
-    public TemperatureUnit TemperatureUnit {
-      get { return temperatureUnit; }
-      set {
-        this.temperatureUnit = value;
-        this.settings.SetValue("TemperatureUnit", (int)temperatureUnit);
-      }
-    }
+    public class UnitManager
+    {
 
-    public static float? CelsiusToFahrenheit(float? valueInCelsius) {
-      return valueInCelsius * 1.8f + 32;
+        private PersistentSettings settings;
+        private TemperatureUnit temperatureUnit;
+
+        public UnitManager(PersistentSettings settings)
+        {
+            this.settings = settings;
+            this.temperatureUnit = (TemperatureUnit)settings.GetValue("TemperatureUnit",
+              (int)TemperatureUnit.Celsius);
+        }
+
+        public TemperatureUnit TemperatureUnit
+        {
+            get { return temperatureUnit; }
+            set
+            {
+                this.temperatureUnit = value;
+                this.settings.SetValue("TemperatureUnit", (int)temperatureUnit);
+            }
+        }
+
+        public static float? CelsiusToFahrenheit(float? valueInCelsius)
+        {
+            return valueInCelsius * 1.8f + 32;
+        }
     }
-  }
 }
